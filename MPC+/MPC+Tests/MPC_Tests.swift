d9e64a5bda38934725795f9a238da902fc79e6be
Dylan Clark <dylanclark51311@gmail.com>
2026-01-05 11:17:04 -0500

    Recreate Xcode project and migrate app to MPC+

---
+++ MPC+/MPC+Tests/MPC_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MPC_Tests.swift
+//  MPC+Tests
+//
+//  Created by Dylan Clark on 1/5/26.
+//
+
+import Testing
+@testable import MPC_
+
+struct MPC_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
